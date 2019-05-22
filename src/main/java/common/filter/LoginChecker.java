b649bc4619ef135c02519bbb20365d2d70c13ca1
psk <psk3060@naver.com>
2019-05-22 20:04:56 +0900

    Login Check 수정

--- src/main/java/common/filter/LoginChecker.java
+++ src/main/java/common/filter/LoginChecker.java
@@ -22,28 +22,35 @@ public class LoginChecker implements Filter {
         HttpServletRequest servletRequest = (HttpServletRequest)request;
 
         HttpSession session = servletRequest.getSession();
+    
+        String requestUri = servletRequest.getRequestURI();
         
         boolean login = false;
 
-        if(session != null) {
-            if( session.getAttribute("LOGIN_USER") != null ) {
-                login = true;
-            }
-        }
-
-        if( login ) {
+        if( requestUri.contains("loginProcessor") ) {
             chain.doFilter(request, response);
         } else {
-            HttpServletResponse servletResponse = (HttpServletResponse)response;
-
-            // 메세지 출력 후 redirectUrl으로 이동시킴
-            String confirm_url = "/webapp/views/confirm.jsp";
-
-            servletRequest.setAttribute("redirectUrl", "/webapp/views/login/login.jsp");
-            servletRequest.setAttribute("msg", "해당 서비스는 로그인 한 유저만이 접근할 수 있습니다.");
-            RequestDispatcher dispatcher = servletRequest.getRequestDispatcher(confirm_url);
-            dispatcher.forward(servletRequest, servletResponse);
+            if(session != null) {
+                if( session.getAttribute("LOGIN_USER") != null ) {
+                    login = true;
+                }
+            }
+    
+            if( login ) {
+                chain.doFilter(request, response);
+            } else {
+                HttpServletResponse servletResponse = (HttpServletResponse)response;
+    
+                // 메세지 출력 후 redirectUrl으로 이동시킴
+                String confirm_url = "/webapp/views/confirm.jsp";
+    
+                servletRequest.setAttribute("redirectUrl", "/webapp/views/login/login.jsp");
+                servletRequest.setAttribute("msg", "해당 서비스는 로그인 한 유저만이 접근할 수 있습니다.");
+                RequestDispatcher dispatcher = servletRequest.getRequestDispatcher(confirm_url);
+                dispatcher.forward(servletRequest, servletResponse);
+            }
         }
+        
     }
 
     public void destroy() {}
