8daf375c80153aca8f6b871c586164239a7a7eb9
psk <psk3060@naver.com>
2019-05-22 17:26:27 +0900

    LoginChecker 미리 작성

--- src/main/java/common/filter/LoginChecker.java
+++ src/main/java/common/filter/LoginChecker.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class LoginChecker implements Filter {
     public void init(FilterConfig fConfig) throws ServletException {
@@ -15,8 +19,31 @@ public class LoginChecker implements Filter {
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest servletRequest = (HttpServletRequest)request;
 
-        chain.doFilter(request, response);
+        HttpSession session = servletRequest.getSession();
+        
+        boolean login = false;
+
+        if(session != null) {
+            if( session.getAttribute("LOGIN_USER") != null ) {
+                login = true;
+            }
+        }
+
+        if( login ) {
+            chain.doFilter(request, response);
+        } else {
+            HttpServletResponse servletResponse = (HttpServletResponse)response;
+
+            // 메세지 출력 후 redirectUrl으로 이동시킴
+            String confirm_url = "/webapp/views/confirm.jsp";
+
+            servletRequest.setAttribute("redirectUrl", "/webapp/views/login/login.jsp");
+            servletRequest.setAttribute("msg", "해당 서비스는 로그인 한 유저만이 접근할 수 있습니다.");
+            RequestDispatcher dispatcher = servletRequest.getRequestDispatcher(confirm_url);
+            dispatcher.forward(servletRequest, servletResponse);
+        }
     }
 
     public void destroy() {}
